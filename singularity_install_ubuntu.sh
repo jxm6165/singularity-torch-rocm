a4ea8ebb973d90ba76eadbea6747f09e110aa430
jxm6165 <35394963+jxm6165@users.noreply.github.com>
2022-10-16 03:29:16 -0400

    Update singularity_install_ubuntu.sh

--- singularity_install_ubuntu.sh
+++ singularity_install_ubuntu.sh
@@ -13,8 +13,7 @@ echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
 echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
 source ~/.bashrc
 
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh |
-sh -s -- -b $(go env GOPATH)/bin v1.21.0
+curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.21.0
 
 mkdir -p ${GOPATH}/src/github.com/sylabs && \
 cd ${GOPATH}/src/github.com/sylabs && \
